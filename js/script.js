ada5fd78eb48dbcb824c495cf32971fc539044d2
Diana Resendiz <diana.resendiz@cceo.com.mx>
2018-03-08 22:58:26 -0600

    creacion de todos los botones necesarios #2

--- js/script.js
+++ js/script.js
@@ -1,150 +1,95 @@
 var cartas = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 var tipos = ["corazon","rombo","trebol","espada"];
 var aux = [];
-var contBar = 1;
+var contBar = 0;
 
-$(document).on('click','.jugar', function (event) {
-    var x = Math.floor((Math.random() * 3 )); //tipos.length
-    var y = Math.floor((Math.random() * 12)); //cartas.length
+$(document).on('click','#iniciar', function (event) {
+    for(var i=0; i<4; i++){
+        do{
+            var x = Math.floor((Math.random() * 4 )); //tipos.length
+            var y = Math.floor((Math.random() * 13)); //cartas.length
+            var nuevo = tipos[x] + "-" + cartas[y];
+            
+        }while(aux.indexOf(nuevo)!=-1);
 
-    var nuevo = tipos[x] + cartas[y];
-    console.log(nuevo);
-});
-$(document).on('click','.repartir', function (event) {
-    if(contBar<=52){
+        aux.push(nuevo);
+        
+        //$("#carCasa1").attr("src","imgs/Cards/"+ aux[0] +".png");
+        $("#carCasa1").attr("src","imgs/Cards/0.jpg");
+        $("#carCasa2").attr("src","imgs/Cards/"+ aux[1] +".png");
+        $("#carJugador1").attr("src","imgs/Cards/"+ aux[2] +".png");
+        $("#carJugador2").attr("src","imgs/Cards/"+ aux[3] +".png");
 
-    
-    do{
-        var x = Math.floor((Math.random() * 4 )); //tipos.length
-        var y = Math.floor((Math.random() * 13)); //cartas.length
-
-        var nuevo = tipos[x] + cartas[y];
-        //console.log(nuevo);
-        //console.log(aux.indexOf(nuevo));
-        //console.log("--------------------------------");
-    }while(aux.indexOf(nuevo)!=-1);
-    $("#carJugador1").attr("src","imgs/Cards/"+ nuevo +".png");
-    console.log(nuevo);
-    console.log(contBar);
-    console.log("--------------------------------");
-    aux.push(nuevo);
-
-    contBar++;
-    }else{
-        //$("#fin").attr("src","imgs/s.jpg");
-        //$("#fin").attr("src","imgs/Cards/"+ nuevo +".jpg");
-        console.log("FINAL");
+        $("#iniciar").hide();  
+        $("#hit").show();  
+        $("#stand").show();  
     }
-    //console.log(aux);
-});
-/*$(document).on('click','.active', function (event) {
-    var opcion = $(this);
-    var type = opcion.data('type');
-    var content = opcion.data('content');
 
-    if(type == 1){
-        $("#Trailer").attr("src","videos/losincreibles2.mp4");
-        $("#Trailer").attr("poster","imgs/li2.jpg");
+});
+$(document).on('click','#hit', function (event) {
+    if(contBar<52){    
+        do{
+            var x = Math.floor((Math.random() * 4 )); //tipos.length
+            var y = Math.floor((Math.random() * 13)); //cartas.length
+
+            var nuevo = tipos[x] + "-" + cartas[y];
+            
+        }while(aux.indexOf(nuevo)!=-1);
+
+        $(".cartasJugador").append("<img id='carCasa1' src='imgs/Cards/"+nuevo+".png' width='100' height='150'>");
+
+        aux.push(nuevo);
+
+        contBar++;
     }else{
-        if(type == 2){
-            $("#Trailer").attr("src","videos/PanteraNegra.mp4");
-            $("#Trailer").attr("poster","imgs/bp.jpg");
+        console.log("ya no hay barajas");
+    }
+});
+$(document).on('click','#stand', function (event) {
+    $("#stand").hide();  
+    $("#hit").hide();  
+    var numBar = contBar+4;
+    
+    var puntosJugador = 0;
+
+    for(var i=2; i<numBar; i++){
+
+        var cadena = aux[i].split("-");
+        
+        if(cadena[1]=="A" || cadena[1]=="J" || cadena[1]=="Q" || cadena[1]=="K"){
+            puntosJugador += 10;
         }else{
-            if(type == 3){
-                $("#Trailer").attr("src","videos/LaBodaDeValentina.mp4");
-                $("#Trailer").attr("poster","imgs/lbdv.jpg");
+            puntosJugador += cartas.indexOf(cadena[1]) + 1;
+        }
+    }
+
+    console.log("-------pts JUGADOR----------");
+    console.log(puntosJugador);
+
+    if(puntosJugador > 21){
+        console.log("GANA LA CASA");        
+    }else{
+        console.log("TURNO DE LA CASA");  
+        
+        var puntosCasa = 0;
+              
+        for(var i=0; i<2; i++){
+
+            var cadena = aux[i].split("-");
+            
+            if(cadena[1]=="A" || cadena[1]=="J" || cadena[1]=="Q" || cadena[1]=="K"){
+                puntosCasa += 10;
             }else{
-                $("#Trailer").attr("src",content);
-                $("#Trailer").attr("poster","imgs/new.png");
+                puntosCasa += cartas.indexOf(cadena[1]) + 1;
             }
         }
+
+        if(puntosCasa <15){
+            
+        }
+
+        console.log("-------pts CASA----------");
+        console.log(puntosCasa);
     }
+
 });
- 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function handleFiles(files) {
-    var file = files[0];
-    //console.log(file);
-    var reader = new FileReader();
-    
-    
-    $("#playlist").append("<li role='presentation'>"+
-        "<img id='movie4' src='imgs//new.png' draggable='true' ondragstart='drag(event)'>"+
-        "<a class='active' data-type='4' data-content="+"'C://ProjectsUAA//videos//MazeRunner.mp4' "+"href='javascript:void(0)' role='menuitem'> &nbsp;&nbsp;"+file.name+
-        "</a></li><br>");
-
-
-}
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-
-    if(data == "movie1"){
-        $("#Trailer").attr("src","videos/losincreibles2.mp4");
-        $("#Trailer").attr("poster","imgs/li2.jpg");
-    }else{
-        if(data == "movie2"){
-            $("#Trailer").attr("src","videos/PanteraNegra.mp4");
-            $("#Trailer").attr("poster","imgs/bp.jpg");
-        }else{
-            if(data == "movie3"){
-                $("#Trailer").attr("src","videos/LaBodaDeValentina.mp4");
-                $("#Trailer").attr("poster","imgs/lbdv.jpg");
-            }else{
-                //Cambiar la direccion del video a la seleccionada
-                $("#Trailer").attr("src","videos/MazeRunner.mp4");
-                $("#Trailer").attr("poster","imgs/new.png");
-            }
-        }
-    }    
- 
-
-    
-    var files = ev.dataTransfer.files;
-    var x = ev.dataTransfer.items;
-    //x[0].w
-    var count = files.length;
-
-    //var x = window.URL.createObjectURL(files[0]);
-    drop_handler(ev);
-    if (count > 0)
-        handleFiles(files);
-
-    //-------------------------------------------------------------------------------
-
-
-}
-function drop_handler(ev) {
-    //console.log("Drop");
-    ev.preventDefault();
-    var data = event.dataTransfer.items;
-    var f = data[0].getAsFile();
-    //console.log("... Drop: File "+f.files);
-
-    // If dropped items aren't files, reject them
-    var dt = ev.dataTransfer;
-    if (dt.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      for (var i=0; i < dt.items.length; i++) {
-        if (dt.items[i].kind == "file") {
-          var f = dt.items[i].getAsFile();
-          //console.log(".... file[" + i + "].name = " + f.name);
-          //console.log(".... file = " + dt.items[i]);
-          //console.log(".... f = " + f);
-        }
-      }
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      for (var i=0; i < dt.files.length; i++) {
-        //console.log("... file[" + i + "].name = " + dt.files[i].name);
-        //console.log("... file ==== " + dt.files);
-      }  
-    }
-  }*/
